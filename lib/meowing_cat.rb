ec7a435cca1f5e8e0eaf4283ca93fa6f7c968f22
Luis Escobar <lae311@stern.nyu.edu>
2021-11-23 11:50:54 -0500

    commit tree f818bcf2103f6ab479412eb3beca480a2eeee52e

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat
+        # readers are returning the code such as a getter
+        # writers are updating the code
+        attr_accessor :name
+
+        def name=(name)
+            @name= name
+        end
+
+        def meow
+            puts "meow!"
+        end
+
+end
